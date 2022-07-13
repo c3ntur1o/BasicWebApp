31c6f58d06c204b4e6e1e02a39b687ada179d4ea
c3ntur1o <44846221+c3ntur1o@users.noreply.github.com>
2022-07-13 15:32:36 +0200

    query handling

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,8 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("number") && query.contains("largest")){
-            String numbers = query.substring(query.indexOf(": ") + 2);
+            String numbers = query.substring(query.indexOf(": "));
+            numbers = numbers.substring(query.indexOf(": ") + 3);
             String[] num = numbers.split(", ");
             Object[] res = Arrays.asList(num).stream().map(s -> Integer.parseInt(s)).sorted().toArray();
             return "" + ((Integer)res[res.length - 1]);
@@ -23,7 +24,11 @@ public class QueryProcessor {
            return "My name is c3ntur1o";
         }
         else if(query.contains("plus")){
+            String numbers = query.substring(query.indexOf("plus ") + 6);
             return "" + 42;
+        } else if(query.contains("multiplied")){
+            //String nubmer2 = query.substring(query.indexOf("multiplied by ") + );
+            return "";
         }
         else { // TODO extend the programm here
             return "";

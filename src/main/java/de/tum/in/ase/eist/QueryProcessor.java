d678df9d8af3c0cac0cb4878580fd7d011b3cf30
c3ntur1o <44846221+c3ntur1o@users.noreply.github.com>
2022-07-13 14:59:28 +0200

    what is your name query

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "c3ntur1o";
         } else { // TODO extend the programm here
             return "";
         }
